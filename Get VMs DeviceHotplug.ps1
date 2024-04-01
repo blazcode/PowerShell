908610b7b3769f673c23d6ede7e9b19c36a62b36
Corey Blaz <58145113+blazcode@users.noreply.github.com>
2024-04-01 09:56:04 -0400

    Update Get VMs DeviceHotplug.ps1

--- Get VMs DeviceHotplug.ps1
+++ Get VMs DeviceHotplug.ps1
@@ -15,15 +15,10 @@ $output = @()
 # Get all VMs and iterate through them, searching for devices.hotplug
 Foreach ($VM in Get-VM) {
     Write-Host "Checking: " $VM.name
-    if ($($VM | Get-AdvancedSetting -Name "devices.hotplug").Value) {
-        $devicesHotplug = $true
-    } else {
-        $devicesHotplug = $false
-    }
 
     $vmObject = New-Object -TypeName PSObject 
     $vmObject | Add-Member -MemberType NoteProperty -Name 'Name' -Value $VM.name
-    $vmObject | Add-Member -MemberType NoteProperty -Name 'devicesHotplug' -Value $devicesHotplug
+    $vmObject | Add-Member -MemberType NoteProperty -Name 'devicesHotplug' -Value $($VM | Get-AdvancedSetting -Name "devices.hotplug").Value 
 
     $output += $vmObject
 }
