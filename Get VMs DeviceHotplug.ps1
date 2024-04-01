89f1fa7379f95b2b14d65ca494855d1a9af0f048
Corey Blaz <58145113+blazcode@users.noreply.github.com>
2024-04-01 12:20:16 -0400

    Update Get VMs DeviceHotplug.ps1
    Get-AdvancedSetting is stupid.

--- Get VMs DeviceHotplug.ps1
+++ Get VMs DeviceHotplug.ps1
@@ -16,13 +16,23 @@ $output = @()
 Foreach ($VM in Get-VM) {
     Write-Host "Checking: " $VM.name
 
+    # Get-AdvancedSetting returns nothing at all if setting is unset, therefore must set to $null and update the value if the setting is configured
+    $devicesHotplugSetting = $null
+    $devicesHotplugSetting = $(Get-AdvancedSetting -Entity $VM -Name "devices.hotplug").Value
+
+    if ($devicesHotplugSetting -ne $null) {
+        $devicesHotplugValue = $devicesHotplugSetting
+    } else {
+        $devicesHotplugValue = "UNSET"
+    }
+
     $vmObject = New-Object -TypeName PSObject 
     $vmObject | Add-Member -MemberType NoteProperty -Name 'Name' -Value $VM.name
-    $vmObject | Add-Member -MemberType NoteProperty -Name 'devicesHotplug' -Value $($VM | Get-AdvancedSetting -Name "devices.hotplug").Value 
+    $vmObject | Add-Member -MemberType NoteProperty -Name 'devicesHotplug' -Value $devicesHotplugValue 
 
     $output += $vmObject
 }
 
 # This can be exported to .csv
-$output | sort -Property devicesHotplug -Descending
+$output | sort -Property devicesHotplug -Descending | Format-Table
 #$output | Export-Csv -Path C:\temp\report.csv 
