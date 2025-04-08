66d02171f940ef99d88c4548250c7610b68c2b62
Corey Blaz <58145113+blazcode@users.noreply.github.com>
2025-04-08 15:40:06 -0400

    Hash combined VM vCenter InstanceUUID and VM InstanceUUIDs

--- Indentify VMs.ps1
+++ Indentify VMs.ps1
@@ -19,20 +19,26 @@ foreach ($vc in $global:DefaultVIServers) {
     $vms = Get-VM -Server $vc
 
     foreach ($vm in $vms) {
+        # Hash combined VM vCenter InstanceUUID and VM InstanceUUIDs to create unique ID
+        $hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+        $hash = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($vCenterInstanceUUID + $vm.ExtensionData.Config.InstanceUuid))
+        $globalIdSha = [System.BitConverter]::ToString($hash)
+
         $vmData = [PSCustomObject]@{
-            vCenterName         = $vc.Name
-            vCenterInstanceUUID = $vCenterInstanceUUID 
-            VM_Name              = $vm.Name
-            VM_MoRef            = $vm.Id #VM MoRef
-            VM_InstanceUUID     = $vm.ExtensionData.Config.InstanceUuid #VM InstanceUUID
-            VM_GlobalUniqueId   = $vCenterInstanceUUID + " + " + $vm.ExtensionData.Config.InstanceUuid #Example
+            vCenterName              = $vc.Name
+            vCenterInstanceUUID      = $vCenterInstanceUUID 
+            VM_Name                  = $vm.Name
+            VM_MoRef                 = $vm.Id #VM MoRef
+            VM_InstanceUUID          = $vm.ExtensionData.Config.InstanceUuid #VM InstanceUUID
+            VM_GlobalUniqueId        = $vCenterInstanceUUID + " + " + $vm.ExtensionData.Config.InstanceUuid #Example for illustrative purposes
+            VM_GlobalUniqueId_SHA256 = $globalIdSha.Replace('-', '')
         }
-        
+
         $output += $vmData
     }
 }
 
-Disconnect-VIServer * -Confirm:$false
+#Disconnect-VIServer * -Confirm:$false
 
 # Display and export the output
 $output | Format-Table -AutoSize
